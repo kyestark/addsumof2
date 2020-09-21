42ea393879409ecbb62e4fa4cb7aaaff9526245f
kyestark <70530144+kyestark@users.noreply.github.com>
2020-09-21 17:37:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var num1 = Number(prompt("Enter the first number:"))
+var num2 = Number(prompt("Enter the second number:"))
+var answer = num1 + num2
+document.body.querySelector("#cool").innerHTML=answer
